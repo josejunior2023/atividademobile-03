959ba6f4a42e4e1ca9d42d5b746e229420d785de
josejunior2023 <jose_junior567@hotmail.com>
2024-10-15 19:53:36 -0300

    Updt

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { SafeAreaView, StyleSheet, TextInput, Button } from "react-native";
 
 const TextInputExample = () => {
-  const [text, onChangeText] = React.useState("");
-  const [number, onChangeTexT] = React.useState("");
+  const [name, onChangeText] = React.useState("");
+  const [password, onChangeTexT] = React.useState("");
   const login = () => {
-    console.log(text, number);
+    console.log(name, password);
   };
 
   return (
@@ -14,12 +14,12 @@ const TextInputExample = () => {
         style={styles.input}
         onChangeText={onChangeText}
         placeholder="Username"
-        value={text}
+        value={name}
       />
       <TextInput
         style={styles.input}
         onChangeText={onChangeTexT}
-        value={number}
+        value={password}
         placeholder="Password"
       />
 
